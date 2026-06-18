8a25420c57da151608a57849ce417054c2cd98e2
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-06-18 18:08:05 +0600

    Login and register page design started

---
+++ src/app/login/page.jsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: "Login - Fable",
+  description:
+    "Fable is a digital platform that connects ebook lovers, readers, and collectors with talented writers. The platform allows users to browse, discover, and read original ebooks. Writers can upload and manage their creations after a one-time verification payment, while an admin oversees the entire system.",
+};
+
+const LoginPage = () => {
+  return (
+    <div>
+      <p>LoginPage</p>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/app/register/page.jsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: "Register - Fable",
+  description:
+    "Fable is a digital platform that connects ebook lovers, readers, and collectors with talented writers. The platform allows users to browse, discover, and read original ebooks. Writers can upload and manage their creations after a one-time verification payment, while an admin oversees the entire system.",
+};
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <p>RegisterPage</p>
+    </div>
+  );
+};
+
+export default RegisterPage;
