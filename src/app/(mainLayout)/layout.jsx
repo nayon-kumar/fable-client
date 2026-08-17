27d31e7a2f637ec50098832e8dc7e5f24d8506f7
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-08-17 17:51:30 +0600

    Different layout added

--- src/app/(mainLayout)/layout.jsx
+++ src/app/(mainLayout)/layout.jsx
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import Navbar from "@/components/shared/Navbar";
 import Footer from "@/components/shared/Footer";
 import { Toaster } from "react-hot-toast";
