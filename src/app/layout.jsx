935d4c55061c9c55a5fa6f5a04a81f2d519cc8cb
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-08-26 20:00:00 +0600

    Project title changed

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Fable - Home",
+  title: "Home - Fabel",
   description:
     "Fable is a digital platform that connects ebook lovers, readers, and collectors with talented writers. The platform allows users to browse, discover, and read original ebooks. Writers can upload and manage their creations after a one-time verification payment, while an admin oversees the entire system.",
 };
